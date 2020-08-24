755dd2d1030053fc41b4611ae3dc60e4484da484
Kundai Clayton <kundaichats@gmail.com>
2020-08-24 12:55:21 +0200

    all auth functions done, with navigation functionality between pages, and a glowing back button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ForgotpasswordComponent } from './components/auth/forgotpassword/forgotpassword.component';
+import { GuestsigninComponent } from './components/auth/guestsignin/guestsignin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'register' },
+  { path: 'user', component: ForgotpasswordComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component:RegisterComponent  },
+  { path: 'guest', component:GuestsigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { ForgotpasswordComponent } from './components/auth/forgotpassword/forgotpassword.component';
+import { GuestsigninComponent } from './components/auth/guestsignin/guestsignin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ForgotpasswordComponent,
+    GuestsigninComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+   
   ],
+  
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
