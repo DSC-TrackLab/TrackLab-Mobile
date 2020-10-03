7059a6efb1e37e219b25d2218a833c2a91e8b4f4
SabeloX <sabelomtw@gmail.com>
2020-10-03 13:53:23 +0200

    UserDashboard component created under user module

--- src/app/_modules/user/user-routing.module.ts
+++ src/app/_modules/user/user-routing.module.ts
@@ -1,9 +1,13 @@
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: '/dashboard', component: UserDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/_modules/user/user.module.ts
+++ src/app/_modules/user/user.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../../angular-material.module';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserDashboardComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
